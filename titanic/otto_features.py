f358ff7101abef3b71f97103c3528ddd3750c88e
ottomatyk <ottoli@tpg.com.au>
2019-09-28 18:34:50 +1000

    CabinClass extraction

--- titanic/otto_features.py
+++ titanic/otto_features.py
@@ -25,4 +25,33 @@ def getTitle(name_col):
     s2 = pd.DataFrame(s1["Other"].str.split(".", n = 1, expand = True))
     s2.columns = ["Title", "Other"]
     
-    return s2["Title"] 
+    return s2["Title"] # Imports
+rt jupytext
+rt pandas as pd
+ karn.ensemble imp RandomForestClassifier
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+
+# +
+# Function to get cabin class
+
+#Fill NAs in Cabin
+df["Cabin"] = df["Cabin"].fillna("0")
+
+#Function
+def extract_cabinclass(cabin): #C123
+    all_classes = ["A", "B", "C", "D", "E", "F"]
+    classes = ""
+    
+    for i in all_classes:
+        if i in cabin:
+            classes = i
+        else: 
+            continue
+    return classes
+
+df["CabinClass"] = df["Cabin"].apply(lambda cabin: extract_cabinclass(cabin))
+
+
+# Cabin Class {A,B,C,D,E,F}
+df["CabinClass"]
